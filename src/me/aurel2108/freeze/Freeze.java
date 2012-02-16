d94b701b236143c2b114f26f7301434f9c265e9e
Aurel Yen <aurel2108@gmail.com>
2012-02-16 16:17:31 +0100

    First release. Hope you'll enjoy !

---
+++ src/me/aurel2108/freeze/Freeze.java
@@ -0,0 +1,69 @@
+package me.aurel2108.freeze;
+
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Freeze extends JavaPlugin {
+	
+	Logger log = Logger.getLogger("Minecraft");
+	public static ArrayList<String> toFreeze = new ArrayList<String>();
+	
+	private FreezePlayerListener playerListener = new FreezePlayerListener();
+	
+	public void onEnable(){
+		log.info("Freeze enabled.");
+		PluginManager pm = getServer().getPluginManager();
+
+        pm.registerEvents(playerListener, this);
+	}
+	
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		Player player = null;
+		if (sender instanceof Player) {
+			player = (Player) sender;
+		}
+	 
+		if (command.getName().equalsIgnoreCase("freeze") && (player.isOp() || player.hasPermission("freeze.freeze"))){
+			if(args.length > 0)
+			{
+				for (String i : args)
+				{
+					for(Player p : Bukkit.getOnlinePlayers())
+					{
+						if(p.getName().toLowerCase().contains(i.toLowerCase()) || p.getName().equalsIgnoreCase(i))
+						{
+							if(toFreeze.size() == 0 || toFreeze.contains(p))
+							{
+								toFreeze.add(p.getName());
+								player.sendMessage(p.getName() + " freezed.");
+							}
+							else
+							{
+								toFreeze.remove(p.getName());
+								player.sendMessage(p.getName() + " unfreezed.");
+							}
+						}
+					}
+				}
+				return true;
+			}
+			else
+				player.sendMessage("Usage : /freeze <player name>"); return false;
+		}
+		return false;
+	}
+ 
+	
+
+	public void onDisable(){
+		log.info("Freeze disabled.");
+	}
+
+}
---
+++ src/me/aurel2108/freeze/FreezePlayerListener.java
@@ -0,0 +1,58 @@
+package me.aurel2108.freeze;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockIgniteEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class FreezePlayerListener implements Listener {
+	
+	@EventHandler
+	public void onPlayerMove(PlayerMoveEvent event)
+	{
+		if(Freeze.toFreeze.contains(event.getPlayer().getName()))
+		{
+			event.setTo(event.getFrom());
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent event)
+	{
+		if(Freeze.toFreeze.contains(event.getPlayer().getName()))
+		{
+			event.setCancelled(true);
+		}
+	}
+	
+	@EventHandler
+	public void onBlockBreak(BlockBreakEvent event)
+	{
+		if(Freeze.toFreeze.contains(event.getPlayer().getName()))
+		{
+			event.setCancelled(true);
+		}
+	}
+	
+	@EventHandler
+	public void onBlockIgnite(BlockIgniteEvent event)
+	{
+		if(Freeze.toFreeze.contains(event.getPlayer().getName()))
+		{
+			event.setCancelled(true);
+		}
+	}
+	
+	@EventHandler	
+	public void onBlockPlace(BlockPlaceEvent event)
+	{
+		if(Freeze.toFreeze.contains(event.getPlayer().getName()))
+		{
+			event.setCancelled(true);
+		}
+	}
+
+}
